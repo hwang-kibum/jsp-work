a7f1ade2b36c70d4283d4f3fe20725b53843e1c0
hwangkibum <40309727+hwang-kibum@users.noreply.github.com>
2024-02-22 11:21:25 +0900

    Create createdir.sh

---
+++ createdir.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir INSTALL
+cd INSTALL
+mkdir 01.SELIUX_X
+mkdir 02.SELIUX_0
+mkdir 03.vada
+mkdir 04.work_script 
+
+cd 01.SELINUX_X
+mkdir jdk
+mkdir mariadb
+mkdir miso_pack
+mkdir package #mariadb,apache rpm pakage
+mkdir script_Rocky8.1.7.0
+mkdir tomcat
