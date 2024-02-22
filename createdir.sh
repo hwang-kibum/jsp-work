b5b9e20b0c42db15bacb87fa2d542d8ba106721a
hwangkibum <40309727+hwang-kibum@users.noreply.github.com>
2024-02-22 11:38:28 +0900

    Update createdir.sh

--- createdir.sh
+++ createdir.sh
@@ -13,6 +13,5 @@ mkdir miso_pack
 mkdir package #mariadb,apache rpm pakage
 mkdir script_Rocky8_v1.7.0
 mkdir tomcat
-PWD=$(pwd)
-echo $PWD
+
 cp ../../installScript/Rocky8/* ../../INSTALL/01.SELINUX_X/script_Rocky8_v1.7.0/
