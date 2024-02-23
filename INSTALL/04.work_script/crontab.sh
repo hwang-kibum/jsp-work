2270eff2db91a43a89e289d2a783d3864da37f02
hwangkibum <40309727+hwang-kibum@users.noreply.github.com>
2024-02-23 15:17:36 +0900

    Update crontab.sh

--- INSTALL/04.work_script/crontab.sh
+++ INSTALL/04.work_script/crontab.sh
@@ -4,6 +4,7 @@ NOW=$(date +"%Y-%m-%d")
 LOG_PATH1=/data/miso/logs/info
 LOG_PATH2=/data/miso/logs/error
 LOG_PATH3=/data/tomcat/logs
+LOG_PATH4=/data/miso_daemon/logs
 #BAK_PATH=/data/backup
 DAYS=190
 
@@ -11,4 +12,4 @@ DAYS=190
 find ${LOG_PATH1}/* -mtime +${DAYS} -exec rm -f {} \;
 find ${LOG_PATH2}/* -mtime +${DAYS} -exec rm -f {} \;
 find ${LOG_PATH3}/* -mtime +${DAYS} -exec rm -f {} \;
-
+find ${LOG_PATH4}/* -mtime +${DAYS} -exec rm -f {} \;
