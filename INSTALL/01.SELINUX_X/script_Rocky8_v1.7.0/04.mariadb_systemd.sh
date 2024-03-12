8a41bdb94f4c2ebc3628e1aad5289f97eab2fd1b
hwangkibum <40309727+hwang-kibum@users.noreply.github.com>
2024-03-12 12:07:28 +0900

    Update 04.mariadb_systemd.sh

--- INSTALL/01.SELINUX_X/script_Rocky8_v1.7.0/04.mariadb_systemd.sh
+++ INSTALL/01.SELINUX_X/script_Rocky8_v1.7.0/04.mariadb_systemd.sh
@@ -52,8 +52,10 @@ sed -i 's|/data/mariadb/data|/data/mariadb/mariadbData|' ${MARIA_HOME}/support-f
 sed -i 's|/data/mariadb/bin/mariadbd|/data/mariadb/bin/mariadbd-safe|' ${MARIA_HOME}/support-files/systemd/mariadb.service
 sed -i 's|TimeoutStartSec=900|TimeoutStartSec=0|' ${MARIA_HOME}/support-files/systemd/mariadb.service
 
-chown mysql:mysql -R ${MARIADB_SET}
+
 cp -arp ${MARIA_HOME}/support-files/systemd/mariadb.service ${MARIADB_SET}
+chown mysql:mysql -R ${MARIADB_SET}/
+ls -ahil ${MARIADB_SET}
 ln -s ${MARIADB_SET}/mariadb.service /usr/lib/systemd/system/
 
 :<<END
