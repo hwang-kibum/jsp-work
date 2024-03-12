b9be0266026059129848863f36520153a5870367
hwangkibum <40309727+hwang-kibum@users.noreply.github.com>
2024-03-12 10:04:32 +0900

    Update 03.tomcat_install_latest.sh

--- INSTALL/01.SELINUX_X/script_Rocky8_v1.7.0/03.tomcat_install_latest.sh
+++ INSTALL/01.SELINUX_X/script_Rocky8_v1.7.0/03.tomcat_install_latest.sh
@@ -17,6 +17,7 @@ mv ${TOMCAT} tomcat;
 cp ${DATA}/tomcat/conf/server.xml ${DATA}/tomcat/conf/server.xml.bak
 
 #config 파일 복사
+:<<END
 yes|cp -av ${INSTALL}miso_conf/server.xml ${TOMCAT_CONF}
 
 sed -i '154d' ${TOMCAT_CONF}server.xml
@@ -24,6 +25,10 @@ sed -i '154d' ${TOMCAT_CONF}server.xml
 
 sed -i '154 i\\t<Context path="/" docBase="'"${DATA}"'/miso/webapps" reloadable="true"/>' ${TOMCAT_CONF}server.xml
 sed -i '155 i\\t<Context path="/editorImage" docBase="'"${DATA}"'/miso/editorImage" reloadable="true"/>' ${TOMCAT_CONF}server.xml
+END
+sed -i '163 i\\t<Context path="/" docBase="'"${DATA}"'/miso/webapps" reloadable="true"/>' ${DATA}/tomcat/conf/server.xml
+sed -i '164 i\\t<Context path="/editorImage" docBase="'"${DATA}"'/miso/editorImage" reloadable="true"/>' ${DATA}/tomcat/conf/server.xml
+sed -i 's|pattern="%h %l %u %t &quot;%r&quot; %s %b" />|pattern="combined" resolveHosts="false" />|' ${DATA}/tomcat/conf/server.xml
 
 
 echo " "
@@ -33,12 +38,16 @@ echo "!!!catalina.sh config start!!!"
 
 
 #catalina.sh 파일 복사
+:<<END
 yes|cp -av ${INSTALL}miso_conf/${CATALINA} ${TOMCAT_HOME}/bin
+END
+cp -arp ${TOMCAT_HOME}/bin/catalina.sh ${TOMCAT_HOME}/bin/catalina.sh.origin
 chmod 755 ${DATA}/tomcat/bin/catalina.sh 
 
 sed -i '125d' ${DATA}/tomcat/bin/catalina.sh
 sed -i '125 i\JAVA_HOME="'"${DATA}"'/java/"' ${DATA}/tomcat/bin/catalina.sh
-
+sed -i '125 i\JAVA_OPTS="-Xms1024m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=256m -XX:NewSize=400m -XX:MaxNewSize=400m -XX:SurvivorRatio=4"' ${DATA}/tomcat/bin/catalina.sh
+sed -i '125 i\CATALINA_OUT="'"${DATA}"'/logs/catalina.out”' ${DATA}/tomcat/bin/catalina.sh
 ls -ahil ${DATA}/tomcat/bin/catalina.sh
 
 #log경로 변경
