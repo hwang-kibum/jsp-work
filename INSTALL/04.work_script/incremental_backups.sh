898b2be9a582c75a2ee5b804d2dd13e9d46b2980
hwangkibum <40309727+hwang-kibum@users.noreply.github.com>
2024-05-27 17:21:44 +0900

    Update incremental_backups.sh

--- INSTALL/04.work_script/incremental_backups.sh
+++ INSTALL/04.work_script/incremental_backups.sh
@@ -5,9 +5,9 @@ PW='Wlfks@09!@#'
 PATH='/data/backup/incremental_backup'
 DAYS=$(/usr/bin/date +%Y-%m-%d)
 
-TMP_DAYS=7
+TMP_DAYS=1
 TMP_MONTH=3
-OLD_DAYS=$(/usr/bin/date -d "${TMP_DAYS} days ago" +"%Y-%m-%d") #N 일 전
+OLD_DAYS=$(/usr/bin/date -d "${TMP_DAYS} days ago" +"%Y-%m-%d") #N일 전
 OLD_MONTH=$(/usr/bin/date -d "${TMP_MONTH} month ago" +"%Y-%m-%d") #N 개월 전
 
 OLDDAY=$OLD_DAYS
@@ -16,13 +16,16 @@ echo ${OLD_DAYS}
 # 선행 작업.
 #mariadb-backup --backup --history --no-lock --user=${ID} --password=${PW} --target-dir=${PATH}/${DAYS}
 
-
 function old_backup_check {
         if [ -d ${PATH}/${OLDDAY} ];then
                 echo "${OLDDAY} exist"
                 backup_ref_basedir
+        elif [ -d ${PATH}/${DAYS} ];then
+                echo "${DAYS} exist....dir"
+
         else
                 echo "${PATH}/${OLDDAY} directory not exist"
+                backup_first
                 exit
         fi
 }
@@ -35,5 +38,9 @@ function backup_ref_basedir {
                 --incremental-basedir=${PATH}/${OLDDAY}
 }
 
+function backup_first {
+        /usr/bin/mariadb-backup --backup --history --no-lock --user=${ID} --password=${PW} --target-dir=${PATH}/${DAYS}
 
+}
 old_backup_check
+
